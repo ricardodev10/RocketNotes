221439e134bd3352446261b65424bed0812c21c5
ricardo <ricardodev10@yahoo.com>
2023-04-17 15:37:53 -0300

    :lipstick: created component header

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
 
 export function Details() {
   return (
     <Container>
-      <h1>Hello World!</h1>
-      <span>Ricardo Junior</span>
+      <Header />
+      <Button title="Voltar" />
     </Container>
   );
 }
