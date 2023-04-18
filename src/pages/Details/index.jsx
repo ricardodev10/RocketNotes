a14248932266ddc2610cdb5c82124e0f857ff6d1
ricardo <ricardodev10@yahoo.com>
2023-04-18 12:28:08 -0300

    :lipstick: feat: created component tag

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -2,6 +2,7 @@ import { Container, Links } from "./styles";
 import { Header } from "../../components/Header";
 import { Button } from "../../components/Button";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
@@ -17,6 +18,11 @@ export function Details() {
             <a href="#">https://www.rocketseat.com.br</a>
           </li>
         </Links>
+      </Section>
+
+      <Section title="Marcadores">
+        <Tag title="express" />
+        <Tag title="nodejs" />
       </Section>
       <Button title="Voltar" />
     </Container>
