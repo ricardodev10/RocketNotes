b01a715b5c8adaf14f3b7a7f039ef9f6efb68961
ricardo <ricardodev10@yahoo.com>
2023-04-19 13:49:05 -0300

    :lipstick: feat: add page new

---
+++ src/pages/New/index.jsx
@@ -0,0 +1,24 @@
+import { Header } from '../../components/Header'
+import { Input } from '../../components/Input'
+
+import { Container, Form } from "./styles";
+
+
+export function New() {
+    return (
+        <Container>
+            <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Criar nota</h1>
+                        <a href="/">voltar</a>
+                    </header>
+                </Form>
+
+                <Input placeholder="Titulo" />
+            </main>
+        </Container>
+    )
+}
