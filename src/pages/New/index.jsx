857f7e79e137371df51f2c81caf539a25cef153d
ricardo <ricardodev10@yahoo.com>
2023-04-20 12:16:17 -0300

    :lipstick: feat: finalized noteitem

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,6 +1,8 @@
 import { Header } from '../../components/Header'
 import { Input } from '../../components/Input'
 import { Textarea } from '../../components/Textarea'
+import { NoteItem } from '../../components/NoteItem'
+import { Section } from '../../components/Section'
 
 import { Container, Form } from "./styles";
 
@@ -20,6 +22,11 @@ export function New() {
 
                 <Input placeholder="Titulo" />
                 <Textarea placeholder="Observações" />
+
+                <Section title="Links úteis">
+                    <NoteItem value="https://www.rocketseat.com.br/" />
+                    <NoteItem isNew placeholder="Novo link" />
+                </Section>
             </main>
         </Container>
     )
