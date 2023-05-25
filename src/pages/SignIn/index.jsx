4f249f62951f302352b9a866cce4b7fbd0431380
ricardo <ricardodev10@yahoo.com>
2023-05-25 15:09:40 -0300

    :sparkles: feat: hooks signin

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,12 +1,24 @@
-import { FiMail, FiLock, FiLogIn } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import { FiMail, FiLock, FiLogIn } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-import { Input } from '../../components/Input';
-import { Button } from '../../components/Button';
+import { useAuth } from "../../hooks/auth";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 import { Container, Form, Background } from "./styles";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -15,26 +27,23 @@ export function SignIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input 
-            placeholder="E-mail" 
-            type="text"
-            icon={FiMail}
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
-        <Input 
-            placeholder="Senha" 
-            type="password"
-            icon={FiLock}
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button 
-            title="Entrar" 
-        />
+        <Button title="Entrar" onClick={handleSignIn} />
 
-        <Link to="/signup">
-            Criar conta
-        </Link>
-
+        <Link to="/signup">Criar conta</Link>
       </Form>
 
       <Background />
