692bc45909c4c271b959c5badab80b544e38e5f4
ricardo <ricardodev10@yahoo.com>
2023-06-02 11:34:41 -0300

    :sparkles: feat: selected tags

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -13,6 +13,19 @@ import { ButtonText } from '../../components/ButtonText'
 
 export function Home() {
     const [tags, setTags] = useState([]);
+    const [tagsSelected, setTagsSelected] = useState([]);
+
+    function handleTagsSelected(tagName) {
+        const alreadySelected = tagsSelected.includes(tagName);
+
+        if (alreadySelected) {
+            const filteredTags = tagsSelected.filter(tag => tag !== tagName);
+            setTagsSelected(filteredTags);
+
+        } else {
+            setTagsSelected(prevState => [...prevState, tagName]);
+        }
+    }
 
     useEffect(() => {
         async function fetchTags() {
@@ -35,7 +48,8 @@ export function Home() {
                 <li>
                     <ButtonText 
                         title="Todos" 
-                        isActive 
+                        onClick={() => handleTagsSelected("all")}
+                        isActive={tagsSelected.length === 0}
                     />
                 </li>
                 {
@@ -43,6 +57,8 @@ export function Home() {
                         <li key={String(tag.id)}>
                             <ButtonText 
                                 title={tag.name} 
+                                onClick={() => handleTagsSelected(tag.name)}
+                                isActive={tagsSelected.includes(tag.name)}
                             />
                         </li>
                     ))
