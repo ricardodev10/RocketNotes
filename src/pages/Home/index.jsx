5e9ea5f91abe1f4be9d68ba61c610373893f1c6a
ricardo <ricardodev10@yahoo.com>
2023-06-22 08:00:33 -0300

    :sparkles: feat: search notes

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -83,7 +83,7 @@ export function Home() {
       <Search>
         <Input
           placeholder="Pesquisar pelo título"
-          onChange={() => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           icon={FiSearch}
         />
       </Search>
