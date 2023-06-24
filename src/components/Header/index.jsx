4d59f8bfe0d09b6d59035d294df9f795965278e7
ricardo <ricardodev10@yahoo.com>
2023-06-24 12:29:14 -0300

    :bug: fix: redirect logout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,21 @@
-import { RiShutDownLine } from 'react-icons/ri'
-import { useAuth } from '../../hooks/auth';
+import { useNavigate } from "react-router-dom";
+import { RiShutDownLine } from "react-icons/ri";
+import { useAuth } from "../../hooks/auth";
 import { api } from "../../services/api";
 import { Container, Profile, Logout } from "./styles";
 
 export function Header() {
   const { signOut, user } = useAuth();
+  const navigation = useNavigate();
 
-  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder;
+  function handleSignOut() {
+    navigation("/");
+    signOut();
+  }
+
+  const avatarUrl = user.avatar
+    ? `${api.defaults.baseURL}/files/${user.avatar}`
+    : avatarPlaceholder;
 
   return (
     <Container>
@@ -19,7 +28,7 @@ export function Header() {
         </div>
       </Profile>
 
-      <Logout onClick={signOut}>
+      <Logout onClick={handleSignOut}>
         <RiShutDownLine />
       </Logout>
     </Container>
